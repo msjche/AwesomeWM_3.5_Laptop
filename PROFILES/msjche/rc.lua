77e1f7f9c3b310925891f459a22ab3e32f6fcf10
msjche <msjche@gmail.com>
2016-07-03 19:09:12 -0700

    removed dups

--- PROFILES/msjche/rc.lua
+++ PROFILES/msjche/rc.lua
@@ -90,14 +90,6 @@ if beautiful.wallpaper then
 end
 -- }}}
 
--- {{{ Wallpaper
-if beautiful.wallpaper then
-    for s = 1, screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
-    end
-end
--- }}}
-
 -- {{{ Tags
 
 tags = 	{
