cd2426c6e3ee42d139782ca5bfb9a6ceb425d46c
MoJo <msjche@gmail.com>
2017-11-12 01:10:37 -0800

    back to 1440p

--- PROFILES/msjche/rc.lua
+++ PROFILES/msjche/rc.lua
@@ -96,7 +96,7 @@ end
 tags = 	{
 --	names = { "➊", "➋", "➌", "➍", "➎", "➏", "➐", "➑", "➒" },
 	names = { "WEB", "POR", "NEW", "IRC", "MUS", "PIR", "MOV", "GAM", "MIS" },
-	layout = { layouts[10], layouts[7], layouts[8], layouts[10], layouts[4], layouts[5], layouts[10], layouts[1], layouts [1] }
+	layout = { layouts[10], layouts[7], layouts[8], layouts[10], layouts[4], layouts[7], layouts[10], layouts[1], layouts [1] }
 		}
 for s = 1, screen.count() do
 	tags[s] = awful.tag(tags.names, s, tags.layout)
